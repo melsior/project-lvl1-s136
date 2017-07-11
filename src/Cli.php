cf9e34dc15f9755cff1c2a252f9521b8420e973a
melsior <melsior@gmail.com>
2017-07-11 16:51:22 +0300

    Second point

---
+++ src/Cli.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * This file contains logic for work with console
+ *
+ * PHP version 7
+ *
+ * @category Function
+ * @package  BrainGames
+ * @author   Alexander Vasilyev <melsior@gmail.com>
+ * @license  https://github.com/project-lvl1-s136/LICENSE MIT
+ * @link     https://github.com/project-lvl1-s136
+ **/
+namespace BrainGames;
+
+use function \cli\line;
+/**
+ * This function for output user greetin in console.
+ *
+ * @return nothing
+ **/
+function run()
+{
+    line('Welcome to the Brain Game!');
+    $name = \cli\prompt('May I have your name?');
+    line("Hello, %s!", $name);
+}
