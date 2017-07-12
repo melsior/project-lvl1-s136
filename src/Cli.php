9e94c3fcbe01c74c4c6698231301058ae44e026e
melsior <melsior@gmail.com>
2017-07-12 23:53:59 +0300

    fourth poin work version

--- src/Cli.php
+++ src/Cli.php
@@ -22,7 +22,32 @@ use function \cli\line;
 function run()
 {
     line('Welcome to the Brain Game!');
-    line('Answer "yes" if number even otherwise answer "no".\n');
     $name = \cli\prompt('May I have your name?');
     line("Hello, %s!", $name);
+}
+/**
+ * This function make even game in console.
+ *
+ * @return nothing
+ **/
+function runEvenGame()
+{
+    line('Welcome to the Brain Game!');
+    line('Answer "yes" if number even otherwise answer "no".');
+    line();
+    $name = \cli\prompt('May I have your name?');
+    line();
+    for ($i = 0; $i < 3; $i++) {
+        $number = getRand();
+        line('Question: %s', $number);
+        $answer = \cli\prompt('Your answer: ');
+        if ($answer !== getRightAnswer($number)) {
+            line('\'%s\' is wrong answer ;(. Correct answer was \'%s\'.', $answer, getRightAnswer($number));
+            line('Let\'s try again, %s!', $name);
+            exit;
+        } else {
+            line('Correct!');
+        }
+    }
+    line('Congratulation, %s!', $name);
 }
---
+++ src/GameLib.php
@@ -0,0 +1,40 @@
+<?php
+/**
+ * This file contains function for use in game with even number
+ *
+ * PHP version 7
+ *
+ * @category Function
+ * @package  BrainGames
+ * @author   Alexander Vasilyev <melsior@gmail.com>
+ * @license  https://github.com/project-lvl1-s136/LICENSE MIT
+ * @link     https://github.com/project-lvl1-s136
+ **/
+namespace BrainGames;
+
+/**
+ * This function for get right answer for even or odd number.
+
+ * @param interger $number random number
+ *
+ * @return string answer 'yes' or 'no'
+ **/
+function getRightAnswer($number)
+{
+    if (($number % 2) == 0) {
+            return 'yes';
+    } else {
+            return 'no';
+    }
+}
+/**
+ * This function generation random number.
+ *
+ * @return random number
+ **/
+function getRand()
+{
+    $min = 1;
+    $max = 100;
+    return rand($min, $max);
+}
